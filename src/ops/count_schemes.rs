0d3184bb49bcb5367e24c16f187e503c3bbcf39e
peterc-s <petercs@purelymail.com>
2025-03-29 15:42:59 +0000

    refactor scheme count and fix a bug in it

--- src/ops/count_schemes.rs
+++ src/ops/count_schemes.rs
@@ -4,33 +4,27 @@ use json::JsonValue;
 use url::Url;
 
 pub fn get_counts(value: &JsonValue, counts: &mut HashMap<String, usize>) {
-    match value {
-        JsonValue::Object(map) => {
-            for (key, val) in map.iter() {
-                // look for "url" fields
-                if key == "url" {
-                    if let JsonValue::String(url) = val {
-                        // parse the url
-                        if let Ok(parsed_url) = Url::parse(url) {
-                            // get the host name
-                            counts
-                                .entry(parsed_url.scheme().to_string())
-                                .and_modify(|count| *count += 1)
-                                .or_insert(1);
-                        }
-                    }
-                }
+    if let JsonValue::Array(entries) = &value["log"]["entries"] {
+        for entry in entries {
+            let url_json = &entry["request"]["url"];
+            let url_str = match url_json {
+                JsonValue::String(s) => Some(s.as_str()),
+                JsonValue::Short(s) => Some(s.as_str()),
+                _ => None,
+            };
 
-                // continue recursively parsing
-                get_counts(val, counts);
-            }
-        },
-        JsonValue::Array(arr) => {
-            for item in arr {
-                // recursively parse each value in the array
-                get_counts(item, counts);
-            }
-        },
-        _ => {},
+            let count_key = match url_str {
+                Some(url) => match Url::parse(url) {
+                    Ok(parsed_url) => parsed_url.scheme().to_string(),
+                    Err(_) => "Bad URL".into(),
+                },
+                None => {
+                    eprintln!("BAD JSON FOR URL: {:?}", url_json);
+                    "Bad JSON".into()
+                }
+            };
+
+            *counts.entry(count_key).or_insert(0) += 1;
+        }
     }
 }
